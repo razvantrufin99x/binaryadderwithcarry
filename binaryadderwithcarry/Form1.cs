2327b7458a3da989ae6f1899e69258545b9057d6
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-02 07:31:11 +0200

    Add project files.

---
+++ binaryadderwithcarry/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace binaryadderwithcarry
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void calculeazasumaabbinar()
+        {
+            binaddc abcr = new binaddc(0, 0, 0, 0);
+            abcr.a = 1;
+            abcr.b = 1;
+            abcr.addcAB();
+            Text = "a= " + abcr.a.ToString() + " + b= " + abcr.b.ToString();
+            Text += " => r=" + abcr.r.ToString() + " : c =" + abcr.c.ToString();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            calculeazasumaabbinar();
+        }
+    }
+}
---
+++ binaryadderwithcarry/binaddc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace binaryadderwithcarry
+{
+    public class binaddc
+    {
+        public int a = 0;
+        public int b = 0;
+        public int c = 0;
+        public int r = 0;
+
+        public binaddc() { }
+
+        public binaddc(int pa, int pb, int pc, int pr)
+        {
+            a = pa;
+            b = pb;
+            c = pc;
+            r = pr;
+        }
+
+        public void addcAB()
+        {
+          
+
+            if (a == 0 && b == 0) {
+                r = 0;
+            }
+            else if (a == 1 && b == 0)
+            {
+                r = 1;
+            }
+            else if (a == 0 && b == 1)
+            {
+                r = 1;
+            }
+            else if (a == 1 && b == 1)
+            {
+                r = 1;
+                c = 1;
+            }
+
+
+        }
+    }
+}
